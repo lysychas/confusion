e397afe105da4799f015e08b89899ec3464bab61
Lukas Lysychas <sayshi108@gmail.com>
2020-06-22 16:18:54 +0300

    Out of rebase

---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,24 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~ DISHES ~~~~~~~~~~~~~~~~~~~~~~~
+
+export const DISHES_LOADING = "DISHES_LOADING";
+export const DISHES_FAILED = "DISHES_FAILED";
+export const ADD_DISHES = "ADD_DISHES";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ COMMENTS ~~~~~~~~~~~~~~~~~~~~~~~
+
+export const ADD_COMMENT = "ADD_COMMENT"; // post
+
+export const ADD_COMMENTS = "ADD_COMMENTS";
+export const COMMENTS_FAILED = "COMMENTS_FAILED";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ PROMOTIONS ~~~~~~~~~~~~~~~~~~~~~~~
+
+export const PROMOS_LOADING = "PROMOS_LOADING";
+export const ADD_PROMOS = "ADD_PROMOS";
+export const PROMOS_FAILED = "PROMOS_FAILED";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~ LEADERS ~~~~~~~~~~~~~~~~~~~~~~~
+
+export const LEADERS_LOADING = "LEADERS_LOADING";
+export const ADD_LEADERS = "ADD_LEADERS";
+export const LEADERS_FAILED = "PROMOS_FAILED";
